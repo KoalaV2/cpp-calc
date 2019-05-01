fecf6f0a0c89beb136837dabe5a01e0f8de05d1a
KoalaV2 <theo.johanson@gmail.com>
2019-05-01 23:59:15 +0200

    added files

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple calculator written in C++ !
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+int main() {
+    int first_number;
+    int second_number;
+    char calc;
+    std::cout << "Please enter your first number! \n :";
+    std::cin >> first_number;
+    std::cout << "Please enter your second number! \n :";
+    std::cin >> second_number;
+    std::cout << "Please enter how you want to calculate it! \n :";
+    std::cin >> calc;
+    std::cout << "\n";
+    switch(calc){
+        case '+':
+            std::cout << '=' << first_number + second_number << "\n\n";
+            break;
+        case '-':
+            std::cout << '=' << first_number - second_number << "\n\n";
+            break;
+        case '*':
+            std::cout << '=' << first_number * second_number << "\n\n";
+            break;
+        case '/':
+            std::cout << '=' << first_number / second_number << "\n\n";
+	    break;
+        default:
+            std::cout << "Error, wrong input!";
+    }
+    return 0;
+}
